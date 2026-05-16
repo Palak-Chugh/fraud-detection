ae1f271794c8c3a8f0c7524eac8aa68da5c3010b
rahuly1390 <er.rahulyadav13@gmail.com>
2026-05-16 17:23:33 +0530

    Updated the app.py for Streamlit

--- ui/app.py
+++ ui/app.py
@@ -18,14 +18,14 @@ tiktoken
 # IMPORTS
 # ==========================================================
 
-import sys
-import os
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 import streamlit as st
 import pandas as pd
-from orchestration.graph import run_pipeline
+#from orchestration.graph import run_pipeline
+
+#from agents.quant_agent import QuantAgent
+#from agents.research_agent import ResearchAgent
+#from agents.auditor_agent import AuditorAgent
+#from agents.crew_setup import run_credit_audit
  
  # Load data
 df = pd.read_csv("data/financials.csv")
@@ -46,7 +46,7 @@ st.title("Agentic Credit and Fraud Assitant")
 
 st.markdown(
     """
-Agentic Credit and Fraud Assitant:
+Agentic AI financial report PDF and ask questions using:
 
 - Retrieval-Augmented Generation (RAG)
 - LangChain
@@ -65,28 +65,28 @@ company_id = st.selectbox(
 company_data = df[df["name"] == company_name].iloc[0].to_dict()
 
 
-if st.button("Run Audit"):
-    result = run_pipeline(company_data)
+# if st.button("Run Audit"):
+#     result = run_pipeline(company_data)
     
-    quant_result = quant.analyze_company(company_id)
+#     quant_result = quant.analyze_company(company_id)
 
-    research_result = research.read_report(
-        f"data/audit_reports/{company_id}.pdf"
-    )
+#     research_result = research.read_report(
+#         f"data/audit_reports/{company_id}.pdf"
+#     )
 
-    final_result = auditor.evaluate(
-       quant_result,
-        research_result
-    )
+#     final_result = auditor.evaluate(
+#         quant_result,
+#         research_result
+#     )
 
-    st.subheader("Quant Analysis")
-    st.json(quant_result)
+#     st.subheader("Quant Analysis")
+#     st.json(quant_result)
 
-    st.subheader("Research Findings")
-    st.json(research_result)
+#     st.subheader("Research Findings")
+#     st.json(research_result)
 
-    st.subheader("Final Recommendation")
-    st.json(final_result)
+#     st.subheader("Final Recommendation")
+#     st.json(final_result)
  
 
 if st.button("Run AI Audit"):
